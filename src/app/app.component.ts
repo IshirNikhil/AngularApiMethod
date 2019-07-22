5957dacf6357d64dad3b958c4061ac3666ee6bf2
Nikhil <nikhil@ishir.com>
2019-07-22 10:45:27 +0530

    adds method to call api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetPostsService } from './get-posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'apiMethods';
+export class AppComponent implements OnInit {
+
+  constructor(private loadPosts: GetPostsService){
+  }
+  
+  posts:any;
+
+  ngOnInit(){
+    this.loadPosts.getPosts().subscribe(data => {
+      this.posts = data
+      console.log(this.posts);
+    }
+  );
+  }
+
+ 
 }
